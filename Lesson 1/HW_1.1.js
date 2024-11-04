d56cd5ffbb160a06222333d1de0ce2c6651e9e73
evgordeeva <gordeevaevgenya@gmail.com>
2024-11-04 16:49:17 +0200

    init commit.

---
+++ Lesson 1/HW_1.1.js
@@ -0,0 +1,42 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua',
+// 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+
+let a = 'hello';
+console.log(a);
+
+let b = 'owu';
+console.log(b);
+
+let c = 'com';
+console.log(c);
+
+let d = 'ua';
+console.log(d);
+
+let e = 1;
+console.log(e);
+
+let g = 10;
+console.log(g);
+
+let h = -999;
+console.log(h);
+
+let i = 123;
+console.log(i);
+
+let j = 3.14;
+console.log(j);
+
+let k = 2.7;
+console.log(k);
+
+let l = 16;
+console.log(l);
+
+let n = true;
+console.log(n);
+
+let o = false;
+console.log(o);
---
+++ Lesson 1/HW_1.2.js
@@ -0,0 +1,15 @@
+// - Створити 3 змінних firstName, middleName, lastName,
+// наповнити їх своїм ПІБ. З'єднати їх в одну змінну person
+// (Не об'єкт, просто за допомоги конкатенації)
+
+let a = 'firstName';
+a='Yevheniia';
+
+let b = 'middleName';
+b='Ivanivna';
+
+let c = 'lastName';
+c='Hordieieva';
+
+let person = `${a} ${b} ${c}`;
+console.log(person);
